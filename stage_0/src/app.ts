772743ce61b74ac7c7d0ec863f2195fca11e2d51
okibe ogomola onmeje <okibeogomola@gmail.com>
2026-03-04 17:47:49 +0100

    feat(board): add board routes to the main application

--- stage_0/src/app.ts
+++ stage_0/src/app.ts
@@ -3,6 +3,7 @@ import swaggerUi from 'swagger-ui-express';
 import swaggerSpec from './config/swagger.js';
 import userRoutes from './modules/user/user.routes.js';
 import authRoutes from './modules/auth/auth.routes.js';
+import boardRoutes from './modules/board/board.routes.js';
 import { globalErrorHandler } from './common/middleware/error.middleware.js';
 const PORT = 8080;
 const app = express();
@@ -16,6 +17,7 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 //Routes
 app.use(userRoutes);
 app.use(authRoutes);
+app.use(boardRoutes);
 
 //global error handler
 app.use(globalErrorHandler);
