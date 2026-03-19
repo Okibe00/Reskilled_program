ae0a12ec2bbfc5219df65c85b8f1f78029a759ac
okibe ogomola onmeje <okibeogomola@gmail.com>
2026-03-19 15:41:10 +0100

    refactor(column): to inject prismaclient as a dependency instead of importing

--- stage_0/src/modules/column/column.service.ts
+++ stage_0/src/modules/column/column.service.ts
@@ -1,24 +1,27 @@
+import { PrismaClient } from '@prisma/client';
 import prisma from '../../config/database.js';
 import { CreateColumnDto, UpdateColumnDto } from './dto/column.dto.js';
 class ColumnService {
+  constructor(private prisma: PrismaClient) {}
+
   async create(data: CreateColumnDto) {
-    return prisma.column.create({
+    return this.prisma.column.create({
       data: data,
     });
   }
-  
+
   async update(id: string, data: UpdateColumnDto) {
-    return prisma.column.update({
+    return this.prisma.column.update({
       where: { id },
       data: data,
     });
   }
 
   async delete(id: string) {
-    return prisma.column.delete({
+    return this.prisma.column.delete({
       where: { id },
     });
   }
 }
 
-export default new ColumnService();
+export default new ColumnService(prisma);
